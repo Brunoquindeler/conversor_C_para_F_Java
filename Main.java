f4e4b39b01fa66911574e91f1364b807c59591d0
Bruno Quindeler <bruno.quindeler@gmail.com>
2021-05-16 02:55:08 -0300

    Fix

--- Main.java
+++ Main.java
@@ -8,13 +8,13 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         do {
-            System.out.printf("Digite a temperatura em Celsius: ");
+            System.out.print("Digite a temperatura em Celsius: ");
             temperaturaC = sc.nextDouble();
 
-            temperaturaF  = ((9 * temperaturaC) / 5) + 32;
+            temperaturaF  = 9.0 * temperaturaC / 5.0 + 32.0;
             System.out.printf("Equivalente em Fahrenheit: %.1f%n", temperaturaF);
 
-            System.out.printf("Deseja repetir (s/n)? ");
+            System.out.print("Deseja repetir (s/n)? ");
             resposta = sc.next().charAt(0);
         } while (resposta == 's');
 
